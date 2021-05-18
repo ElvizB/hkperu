ca41c7b880c0fa0a356822e04fbcff8f2eb27ff6
ElvizB <elvisbarbaron@hotmail.com>
2021-05-18 01:52:25 -0500

    version6

--- app/config.inc.php
+++ app/config.inc.php
@@ -4,22 +4,22 @@
 //define("SERVIDOR", "http://localhost/eyt");
 //define("SERVIDOR", "https://galatelperu.com/");
 //rutas de la web
-define("SERVIDOR", "https://hkperu.herokuapp.com");
-define("RUTA_SERVICIOS", SERVIDOR."/servicios");
-define("RUTA_CONSULTORIA", SERVIDOR."/consultoria");
-define("RUTA_FORMACION", SERVIDOR."f/ormacion-especializada");
-define("RUTA_NOSOTROS", SERVIDOR."/nosotros");
-define("RUTA_CONTACTO", SERVIDOR."/contacto");
-define("RUTA_SMARTCITIES", SERVIDOR."/smart-cities");
-define("RUTA_SMARTGRIDS", SERVIDOR."/smart-grids");
-define("RUTA_DIGITAL", SERVIDOR."/transformacion-digital");
-define("RUTA_AGILIDAD", SERVIDOR."/agilidad");
-define("RUTA_SAP", SERVIDOR."/sap");
-define("RUTA_PMO", SERVIDOR."/pmo");
+define("SERVIDOR", "https://hkperu.herokuapp.com/");
+define("RUTA_SERVICIOS", SERVIDOR."servicios");
+define("RUTA_CONSULTORIA", SERVIDOR."consultoria");
+define("RUTA_FORMACION", SERVIDOR."formacion-especializada");
+define("RUTA_NOSOTROS", SERVIDOR."nosotros");
+define("RUTA_CONTACTO", SERVIDOR."contacto");
+define("RUTA_SMARTCITIES", SERVIDOR."smart-cities");
+define("RUTA_SMARTGRIDS", SERVIDOR."smart-grids");
+define("RUTA_DIGITAL", SERVIDOR."transformacion-digital");
+define("RUTA_AGILIDAD", SERVIDOR."agilidad");
+define("RUTA_SAP", SERVIDOR."sap");
+define("RUTA_PMO", SERVIDOR."pmo");
 
 //recursos
-define("RUTA_CSS", SERVIDOR . "/css/");
-define("RUTA_JS", SERVIDOR . "/js/");
-define("RUTA_IMG", SERVIDOR . "/images/");
-define("RUTA_VIDEO", SERVIDOR . "/video/");
+define("RUTA_CSS", SERVIDOR . "css/");
+define("RUTA_JS", SERVIDOR . "js/");
+define("RUTA_IMG", SERVIDOR . "images/");
+define("RUTA_VIDEO", SERVIDOR . "video/");
 
